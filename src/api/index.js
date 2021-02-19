e9a271a66e2b56a12485a20529f08cc6f30cbc77
易长松 <xywc_s@foxmail.com>
2021-02-19 17:30:59 +0800

    计划任务添加和查询

--- src/api/index.js
+++ src/api/index.js
@@ -6,4 +6,7 @@ export const allViews = (params) => request.get('', { params: params });
 
 export const keywordDetail = (key) => request.get(`/keyword/${key}`);
 
+export const createJob = (params) => request.post('/job', params);
+export const getAllJobs = () => request.get('/job');
+
 
--- src/views/Job.vue
+++ src/views/Job.vue
@@ -3,31 +3,32 @@
     <v-data-table :headers="headers" :items="desserts" sort-by="strategy">
       <template v-slot:top>
         <v-toolbar flat color="white">
-          <v-toolbar-title>计划任务</v-toolbar-title>
-          <v-divider class="mx-4" inset vertical></v-divider>
+          <caption>计划任务</caption>
+          <!-- <v-divider class="mx-4" inset vertical></v-divider> -->
           <v-spacer></v-spacer>
           <v-dialog v-model="dialog" width="600px" max-width="90%">
             <template v-slot:activator="{ on, attrs }">
-              <v-btn disabled color="primary" class="mb-2" v-bind="attrs" v-on="on">添加计划任务</v-btn>
+              <v-btn color="primary" class="mb-2" v-bind="attrs" v-on="on">添加计划任务</v-btn>
             </template>
             <v-card>
               <v-card-title>
-                <span class="headline">添加计划任务</span>
+                <span class="headline">添加任务</span>
               </v-card-title>
 
               <v-card-text>
                 <v-container>
                   <v-row>
                     <v-col cols="12">
-                      <v-text-field v-model="editedItem.name" label="关键词"></v-text-field>
+                      <v-text-field v-model="editedItem.keyword" label="关键词"></v-text-field>
                     </v-col>
                     <v-col cols="12" md="6">
                       <v-select
                         v-model="editedItem.strategy"
                         :hint="`${editedItem.strategy.description}`"
-                        :items="items"
+                        :items="strategies"
                         item-text="name"
                         label="策略"
+                        disabled
                         persistent-hint
                         return-object
                       ></v-select>
@@ -42,23 +43,30 @@
               <v-card-actions>
                 <v-spacer></v-spacer>
                 <v-btn color="blue darken-1" text @click="close">取消</v-btn>
-                <v-btn color="blue darken-1" text @click="save">保存</v-btn>
+                <v-btn :disabled="!formIsValid" color="blue darken-1" text @click="save">保存</v-btn>
               </v-card-actions>
             </v-card>
           </v-dialog>
         </v-toolbar>
       </template>
+
+      <template v-slot:item.strategy="{ item }">{{strategies_descriptions[item.strategy]}}</template>
+      <template v-slot:item.status="{ item }">{{job_status[item.status]}}</template>
+      <template
+        v-slot:item.progress="{ item }"
+      >{{item.progress ? `${item.progress}/${item.cycle}`: item.progress}}</template>
+      <template
+        v-slot:item.created_at="{ item }"
+      >{{$dayjs(item.created_at).format('YYYY-MM-DD HH:mm')}}</template>
       <template v-slot:item.actions="{ item }">
         <v-icon small @click="deleteItem(item)">mdi-delete</v-icon>
-      </template>
-      <template v-slot:no-data>
-        <v-btn color="primary" @click="initialize">Reset</v-btn>
       </template>
     </v-data-table>
   </section>
 </template>
 
 <script>
+import { createJob, getAllJobs } from '@/api';
 export default {
   data: () => ({
     dialog: false,
@@ -67,18 +75,17 @@ export default {
         text: '关键词',
         align: 'start',
         sortable: false,
-        value: 'name',
+        value: 'keyword',
       },
       { text: '策略', value: 'strategy' },
-      { text: '周期', value: 'cycle' },
-      { text: '状态', value: 'status' },
+      { text: '周期(天)', value: 'cycle' },
+      { text: '状态', value: 'status', filterable: true },
       { text: '进度', value: 'progress' },
-      { text: '创建时间', value: 'created_at' },
-      { text: '操作', value: 'actions', sortable: false },
+      { text: '创建时间', value: 'created_at' }
     ],
     desserts: [],
     editedItem: {
-      name: '',
+      keyword: '',
       strategy: {
         value: 0,
         name: '有效追踪',
@@ -87,7 +94,7 @@ export default {
       cycle: 7,
     },
     defaultItem: {
-      name: '',
+      keyword: '',
       strategy: {
         value: 0,
         name: '有效追踪',
@@ -95,23 +102,36 @@ export default {
       },
       cycle: 7,
     },
+    strategies: [{
+      value: 0,
+      name: '有效追踪',
+      description: '第一次完整抓取所有数据，后续对有销量的数据进行数据跟踪'
+    }, {
+      value: 1,
+      name: '完整遍历',
+      description: '每次都完整抓取所有产品'
+    }, {
+      value: 2,
+      name: '综合捕获',
+      description: '每间隔2天重新进行一次完整遍历'
+    }],
+    job_status: ['等待', '进行中', '已完成']
   }),
 
   computed: {
-    items() {
-      return [{
-        value: 0,
-        name: '有效追踪',
-        description: '第一次完整抓取所有数据，后续对有销量的数据进行数据跟踪'
-      }, {
-        value: 1,
-        name: '完整遍历',
-        description: '每次都完整抓取所有产品'
-      }, {
-        value: 2,
-        name: '综合捕获',
-        description: '每间隔2天重新进行一次完整遍历'
-      }]
+    strategies_descriptions() {
+      return this.strategies.map(item => { return item.name })
+    },
+    formIsValid() {
+      return (
+        this.editedItem.keyword &&
+        this.editedItem.cycle
+      )
+    },
+    display_columns() {
+      let columns = ['keyword', 'strategy', 'cycle', 'stauts', 'progress', 'created_at']
+      columns.push('actions')
+      return columns
     }
   },
 
@@ -123,33 +143,18 @@ export default {
 
   mounted() {
     this.initialize()
+    if (this.$route.query.admin) this.headers.push({ text: '操作', value: 'actions', sortable: false })
   },
 
   methods: {
-    initialize() {
-      this.desserts = [
-        {
-          name: 'Frozen Yogurt',
-          strategy: 159,
-          cycle: 6.0,
-          status: 24,
-          progress: 4.0,
-          created_at: this.$dayjs().format('YYYY-MM-DD HH:mm')
-        },
-        {
-          name: 'Ice cream sandwich',
-          strategy: 237,
-          cycle: 9.0,
-          status: 37,
-          progress: 4.3,
-          created_at: this.$dayjs().format('YYYY-MM-DD HH:mm')
-        }
-      ]
+    async initialize() {
+      const data = await getAllJobs()
+      this.desserts = data
     },
 
     deleteItem(item) {
       const index = this.desserts.indexOf(item)
-      confirm('Are you sure you want to delete this item?') && this.desserts.splice(index, 1)
+      confirm('确认删除这项任务吗?') && this.desserts.splice(index, 1)
     },
 
     close() {
@@ -159,10 +164,13 @@ export default {
       })
     },
 
-    save() {
-      //TODO: 添加
-      this.editedItem.cycle = this.editedItem.cycle.name
-      this.desserts.push(this.editedItem)
+    async save() {
+      let params = Object.assign({}, this.editedItem)
+      params.strategy = params.strategy.value
+      params.keyword = params.keyword.trim().toLowerCase()
+      console.log(params);
+      await createJob(params)
+      await this.initialize()
       this.close()
     },
   },
